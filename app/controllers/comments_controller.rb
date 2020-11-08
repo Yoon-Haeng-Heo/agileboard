5ab1eba2639ebef56d6c4fe03efcbd88a10bdaa8
Itsbeeanlongday <dbtjdals1771@ajou.ac.kr>
2020-11-09 02:33:18 +0900

    댓글 기능 수정 완료

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,3 +30,34 @@ class CommentsController < ApplicationController
       @post = Post.find_by(id: params[:comment][:commentable_id])
     end
 end
+
+=begin 대댓글기능
+
+https://dev.to/lucysuddenly/nested-comments-in-ruby-on-rails-1k1f
+
+before_action :find_commentable, only: :create
+
+    def new
+      @comment = Comment.new
+    end
+
+    def create
+      @commentable.comments.build(comment_params)
+      @commentable.save
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+    def find_commentable
+      if params[:comment_id]
+        @commentable = Comment.find_by_id(params[:comment_id]) 
+      elsif params[:post_id]
+        @commentable = Post.find_by_id(params[:post_id])
+      end
+    end
+    
+=end

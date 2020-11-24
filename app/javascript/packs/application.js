223ab3f869f49922d8f338cc64a356f8854c9aa0
Koo minsoo <rnalstn0507@gmail.com>
2020-11-24 17:55:41 +0900

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("channels")
+require("jquery")
 require("chartkick")
 require("chart.js")
 
--- db/migrate/20201105175252_add_column_to_project.rb
+++ db/migrate/20201105175252_add_column_to_project.rb
@@ -1,6 +1,6 @@
 class AddColumnToProject < ActiveRecord::Migration[6.0]
   def change
-    add_column :projects, :start_at, :datetime
-    add_column :projects, :end_at, :datetime
+    # add_column :projects, :start_at, :datetime
+    # add_column :projects, :end_at, :datetime
   end
 end
